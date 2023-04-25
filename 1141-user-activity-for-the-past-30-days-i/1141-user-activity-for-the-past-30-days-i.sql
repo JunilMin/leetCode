f61d12af6d1354f7224370e98aec9a33078dbc5a
alswnsdlf <86759172+alswnsdlf@users.noreply.github.com>
2023-04-25 11:55:28 +0900

    Time: 846 ms (60.04%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,4 +3,4 @@
 select distinct a.activity_date as day, count(distinct a.user_id) as active_users
 from Activity a
 where '2019-06-27' < a.activity_date and '2019-07-27' >= a.activity_date 
-group by day
+group by day;
